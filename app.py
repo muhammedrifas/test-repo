6a9a4c739198e9877360ad9e186bf61887766eaf
Muhammed Rifas <muhammedrifas123@gmail.com>
2022-08-02 11:03:33 +0530

    "separated db.init_app and db.create_all to separate file run.py"

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_jwt import JWT
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
-from db import db
 from resources.store import Store, StoreList
 
 # from models.item import ItemModel
@@ -19,12 +18,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -33,5 +26,4 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-db.init_app(app)
 app.run(port=5000)
