717196fb6775805dc43b13bab2fd7f2ad9e25008
Muhammed Rifas <muhammedrifas123@gmail.com>
2022-08-02 09:02:04 +0530

    Added all files

---
+++ resources/item.py
@@ -0,0 +1,53 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+from code.models.item import ItemModel
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True)
+    parser.add_argument('store_id', type=int, required=True)
+
+    @jwt_required()
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            return item.json(), 200
+        return {'message': "Item not found"}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        request_data = Item.parser.parse_args()
+        item = ItemModel(name, request_data['price'], request_data['store_id'])
+        try:
+            item.save_to_db()
+        except:
+            return {'message': 'An error occurred inserting the item.'}, 500
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {'message': 'Item deleted'}, 200
+        return {'message': 'Item not found!'}, 404
+
+    def put(self, name):
+        item = ItemModel.find_by_name(name)
+        request_data = Item.parser.parse_args()
+        if item is None:
+            item = ItemModel(name, request_data['price'], request_data['store_id'])
+            item.save_to_db()
+            return item.json(), 201
+        else:
+            item.price = request_data['price']
+            item.store_id = request_data['store_id']
+            item.save_to_db()
+            return item.json(), 200
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': [item.json() for item in ItemModel.query.all()]}
