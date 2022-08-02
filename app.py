27ed1c1b0e9fed95d37336f0968e6d252df9facb
Muhammed Rifas <muhammedrifas123@gmail.com>
2022-08-02 10:47:37 +0530

    "moved db.init_app() outside __main__"

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from resources.item import Item, ItemList
 from db import db
 from resources.store import Store, StoreList
 
-#from models.item import ItemModel
-#from models.user import UserModel
+# from models.item import ItemModel
+# from models.user import UserModel
 
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
@@ -33,6 +33,5 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-if __name__ == '__main__':
-    db.init_app(app)
-    app.run(port=5000)
+db.init_app(app)
+app.run(port=5000)
